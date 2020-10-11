11da1e44db34b267f6154d6950733d5597cc7fcd
xiaogouguohe <862839270@qq.com>
2020-10-12 00:32:57 +0800

    commit

--- src/raft/test.sh
+++ src/raft/test.sh
@@ -2,7 +2,8 @@
 rm out
 while [ 1 ]
 do
-  go test -run TestFigure8Unreliable2C >> out
+  #go test -run TestFigure8Unreliable2C >> out
+  go test -run 2B >> out
   sleep 1s
   echo "sleep for a while\n" >> out
 done
